21835543075db2cd3a1632dd3b576d0b2202ba96
ruchir saxena <ruchirsaxena@gmail.com>
2018-06-13 00:46:27 +0530

    Two Way binding and different ways of using events in React

---
+++ src/Two_WayBindingAndEvents.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+
+class Two_WayBindingAndEvents extends Component{
+    constructor(){
+        super();
+        this.state={
+            someVal:"Hello From React"
+        }
+    }
+
+    updateData=(newValue)=>{
+        debugger;
+       let updatedValue="";
+        if (typeof newValue==='string'){
+           updatedValue=newValue;
+       }else{
+           //getting the value of textbox 
+           updatedValue = newValue.target.value;
+       }
+    
+        this.setState({
+            someVal: updatedValue
+        });
+    }
+    render(){
+        return(
+            <div className="container">
+                <br/>
+                <input type="button" className="btn btn-success" onClick={this.updateData.bind(this,"Start journey for React1")} value="Update Data Way1"/>
+                <span>======</span>
+                <input type="button" onClick={() => this.updateData("Start journey for React2")} value="UpdateData Way2" className="btn btn-primary" /><br /> <br /> 
+                <input type="text" className="form-control" onChange={this.updateData} value={this.state.someVal}/>
+                <div>{this.state.someVal}</div>
+            </div>
+        );
+    }
+
+}
+
+export default Two_WayBindingAndEvents;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
 import ToDoList from './ToDoList/ToDoList';
+import Two_WayBindingAndEvents from "./Two_WayBindingAndEvents";
 
-ReactDOM.render(<ToDoList/>
+ReactDOM.render(<Two_WayBindingAndEvents/>
     ,document.getElementById('root'));
 
